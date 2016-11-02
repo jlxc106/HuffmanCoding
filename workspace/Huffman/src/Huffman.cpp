9e9200f2db207fea756e9fd1420159efb4c55707
jlxc106 <jaylim106@gmail.com>
2016-11-01 19:18:50 -0700

    file recovery data loss fix

--- workspace/Huffman/src/Huffman.cpp
+++ workspace/Huffman/src/Huffman.cpp
@@ -72,9 +72,20 @@ public:
 		root = NULL;
 	};
 
-	~HuffmanTree(){};
+	~HuffmanTree(){
+		deleteNode(root);
+	};
 
-
+	void deleteNode(node *N){
+		if(N->leftchild !=NULL){
+			deleteNode(N->leftchild);
+		}
+		if(N->rightchild !=NULL){
+			deleteNode(N->rightchild);
+		}
+		delete N;
+		N = NULL;
+	}
 
 	//global variables
 	int buffer[8]={ 0 };
@@ -184,7 +195,6 @@ public:
 	void printConv(fstream &write_file){
 		write_file << myvec.size() <<"\n";
 		printConv(root, "", write_file);
-		//write_file << "\n";
 	}
 
 /*
@@ -245,7 +255,7 @@ public:
 	 * Description: Method called in filewrite - manages the buffer
 	 * Returns: void
 	 */
-	void writeBuffer(char c, fstream &write_file){ // void? params?
+	void writeBuffer(char c, fstream &write_file){
 		//look up conversion here
 		string temp;
 		for(unsigned int i=0; i<myvec.size(); i++){
@@ -274,13 +284,11 @@ public:
 	void debuffer(fstream &write_file){
 		int temp=0;
 		static int aay=0;
-//		cout << "run: "<<aay<<endl;
 		for(int i=0; i< 8; i++){
 //			cout << "buffer[i]: "<< buffer[i]<<endl;
 			temp += (buffer[i]*pow(2, (7-i)));
 			buffer[i]=0;
 		}
-//		cout << "temp: " << temp << endl;
 		write_file << char(temp);
 		aay++;
 	}
@@ -293,19 +301,35 @@ public:
 class HuffmanDecode{
 public:
 	HuffmanDecode(){
-		//conv_table = new vector;
 		root = NULL;
+		buffer_position=0;
+		for(unsigned int i=0; i<7;i++){
+			buffer[i]=0;
+		}
 	};
 
-	~HuffmanDecode(){};
+	~HuffmanDecode(){
+		deleteNode(root);
+	};
+
+	void deleteNode(node* N){
+		if(N->leftchild != NULL){
+			deleteNode(N->leftchild);
+		}
+		if(N->rightchild != NULL){
+			deleteNode(N->rightchild);
+		}
+		delete N;
+		N = NULL;
+	}
+
+
 	priority_queue<node*, vector<node*>, OrderByFreq> mypq;
 	node *root;
-	vector <node*> conv_table;
-	int buffer[8]={0};
-	int buffer_position=0;
+	int buffer[8];
+	int buffer_position;
 
 	void build(){
-		cout << "mypq.size(): "<<mypq.size() << endl;
 		while(mypq.size() > 1){
 			node *a = mypq.top();	//left child - 0
 			mypq.pop();
@@ -323,41 +347,54 @@ public:
 	 *
 	 *
 	 */
-	void readtable(string s){
-		//node *temp;
-		//char c = s[0];
+	void readtable(fstream &write_file, string s){
 		int freq = 0;
 		string conv_code="";
-		for(unsigned int i=2; i< s.size(); i++){
-			//int a = s[i]-'0';
-			freq =freq*10 + (s[i]-'0');
-			//conv_code += s[i];
+		if(s.size()==0){	//corner case for new line
+			//cout << "entered selective if statement"<<endl;
+			string new_s;
+			s[0]='\n';
+			getline(write_file, new_s);
+			for(unsigned int i=1; i<new_s.size();i++)
+				freq = freq*10 + (new_s[i]-'0');
+		}
+		else{
+			for(unsigned int i=2; i< s.size(); i++){
+				freq =freq*10 + (s[i]-'0');
+			}
 		}
 		node *temp = new node(s[0], freq);
-		conv_table.push_back(temp);
+		//conv_table.push_back(temp);
 		mypq.push(temp);
 		cout << "c: "<< s[0]<< " freq: " << freq <<endl;
 	}
 
 	void decode(string s, fstream &write_file){
-		string code;
+		bool broken = false;
 		node *temp = root;
 		for(unsigned int i=0; i<s.size();i++){
 			//convert s[i] to int
 			unsigned int wut = (unsigned int)(unsigned char)s[i];
-//			cout << wut<<endl;
 			for(unsigned int j=0; j<8; j++){
 				buffer[j]=wut/(pow(2,7-j));
-//				cout << "Buffer[j]: "<< buffer[j]<<endl;
+				if(buffer[j] > 1 || buffer[j] < 0){
+					//cout << "i: "<< i << " j: " << j << " WTF" << endl;
+					cout << "Buffer[j]: "<< buffer[j] <<endl;
+				}
 				wut = wut % (int)pow(2,7-j);
 			}
 			for(unsigned int k=0; k<8; k++){
 				if(buffer[k]==0){
+					cout << "still broken?"<<endl;
 					//go left
 					if(temp->leftchild->key != '\0'){
-						if(temp->leftchild->key !=(char)3)
-							write_file << temp->leftchild->key;
-//						cout << temp->leftchild->key;
+						if(temp->leftchild->key ==(char)3){
+							broken = true;
+							cout << "broken" << endl;
+							break;
+						}
+						//write_file.put(temp->leftchild->key);
+						write_file << temp->leftchild->key;
 						temp = root;
 					}
 					else
@@ -366,16 +403,21 @@ public:
 				else{
 					//go right
 					if(temp->rightchild->key != '\0'){
-						if(temp->rightchild->key!= (char)3)
-							write_file << temp->rightchild->key;
-//						cout << temp->rightchild->key;
+						if(temp->rightchild->key== (char)3){
+							cout <<"broken"<<endl;
+							broken = true;
+							break;
+						}
+						//write_file.put(temp->leftchild->key);
+						write_file << temp->rightchild->key;
 						temp = root;
 					}
 					else
 						temp = temp->rightchild;
 				}
 			}
-			//decode_char(s[i], write_file);
+			if(broken)
+				break;
 		}
 	}
 
@@ -411,13 +453,13 @@ int main(int argc, char *argv[]){
 
 	HuffmanTree hf;
 	string input="hello.txt", output="hellocompress.txt", new_input="hello_new.txt";
-//	string input="big.txt", output="bigcompress.txt", new_input= "big_new.txt";
+//	string input="big.txt", output="bigcompress.txt", new_input="big_new.txt";
 	string line;
 	fstream writefile;
-	fstream uncompressfile;	//duplicate file of readfile
+	fstream uncompressfile;	//duplicate file of readfile - remake of original file
 	ifstream readfile; // ifstream - stream class to read from files
 
-/*
+/*uncomment later
 	if(argc < 4){
 		cout << "Enter a file to read from: ";
 		cin >> input;
@@ -433,70 +475,123 @@ int main(int argc, char *argv[]){
 	}
 */
 
+	//opens three files
 	writefile.open(output.c_str());
 	readfile.open(input.c_str());	// string1.c_str() converts strings into const char*
 	uncompressfile.open(new_input.c_str());
 
+	//			Encoding
 	readfile.seekg(0, ios::end);
 	int readfile_length = readfile.tellg();
 	readfile.seekg(0, ios::beg);
 
+	//Read from the original file to create Huffman Tree
 	char *buffer = new char[readfile_length];
-
 	readfile.read(buffer, readfile_length);
-
 	hf.readString((string)buffer);
 
 	//transfer to priority queue here
 	hf.transferDB();
-
 	hf.build();	//build the huffman tree
 
-
+	//prints the header table - in compressed file
 	hf.printConv(writefile);
 
-	//hf.getit();
+	int write_length1 = writefile.tellg();
+//	cout << write_length1<<endl;
 
+	//reset the file iterator to beginning and start writing the compressed file
 	readfile.clear();
 	readfile.seekg(0, ios::beg);
-
 	hf.filewrite((string)buffer, writefile);
 
-	//writes the compressed file
-//	while(getline(readfile, line)){
-//		hf.filewrite(line, writefile);
-//		writefile << "\n";
-//	}
+	int write_length2 = writefile.tellg();
+//	cout << write_length2<<endl;
 
+
+	//			Decoding
+/*
 	writefile.clear();
-	writefile.seekg(0, ios::beg);
+	writefile.seekg(0, ios::end);
+	int writefile_length2 = writefile.tellg();
+
+*/
+//	writefile.clear();
+	writefile.seekg(0, writefile.beg);
 
 	HuffmanDecode hd;
-	//read from the compressed file to make original file from scratch.
-	//getline(writefile, line);
+	//read only the very first line(contains the size of Huffman table)
 	getline(writefile,line);
-	int number_lines = atoi(line.c_str());
+	unsigned int number_lines = atoi(line.c_str());
 
+	//read the Huffman table
 	for(unsigned int i=0; i<number_lines; i++){
 		getline(writefile,line);
-		hd.readtable(line);
+		hd.readtable(writefile, line);
 	}
 
-	//hd.poptest();
+	hd.build();	//builds the huffman tree
 
-	hd.build();
 
-	getline(writefile, line);
-	hd.decode(line, uncompressfile);
-//	while(getline(writefile,line)){
-		//cout << "line: "<<line << endl;
+
+	//writefile.gcount();
+
+
+
+//	int writefile_length1 = writefile.tellg();
+
+//	int write_length = (write_length2 - write_length1);
+
+//	char *buffer2 = new char[write_length];
+//	writefile.read(buffer2, (write_length));
+
+
+
+
+//	cout << buffer2<<endl;
+
+	string S = "";
+
+//	writefile >> S;
+//	hd.decode(S, uncompressfile);
+
+	while(getline(writefile, line)){
+		S.append(line.append("\n"));
+	}
+	hd.decode(S, uncompressfile);
+	cout << S << endl;
+
+
+	//hd.decode(line, uncompressfile);
+
+//	hd.decode((string)buffer2, uncompressfile);
+
+//	cout<<writefile.peek()<<endl;
+
+	//hf.getit();
+	//hd.getit();
+
+
+
+
+
+//
+//	while(getline(writefile, line)){
 //		hd.decode(line, uncompressfile);
-		//uncompressfile <<"\n";
 //	}
 
-	//hd.getit();
+
+
+
+
+
+
 
 	uncompressfile.close();
 	readfile.close();
 	writefile.close();
+
+
+	delete buffer;
+//	delete buffer2;
 }
